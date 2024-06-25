2dc4478bc3e1a915ca84463af86a1dfcc644ab63
Fernando Bermudez <Fernando.Bermudez@hunterindustries.com>
2024-06-25 08:15:43 -0700

    Finished setting up canvas. Started button to add rotors.

--- my-design/src/components/Board.vue
+++ my-design/src/components/Board.vue
@@ -1,6 +1,8 @@
 <template>
   <h1>canvas</h1>
+  <button>Add Rotor </button>
   <canvas ref="canvas" width="200" height="200"></canvas>
+
 </template>
 
 <script setup lnag="ts">
@@ -8,7 +10,14 @@ import { fabric } from 'fabric'
 import { ref, onMounted } from 'vue'
 
   const canvas = ref(null)
+  const width = ref(window.innerWidth)
+  const height = ref(window.innerHeight)
+
   onMounted(() => {
     const c = new fabric.Canvas(canvas.value)
+    c.setWidth(width.value)
+    c.setHeight(height.value)
+    console.log(width.value)
+    console.log(height.value)
   })
 </script>
