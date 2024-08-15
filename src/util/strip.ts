18a00ca4bfcf91512aacac575de94b911e7df513
Fernando Bermudez <Fernando.Bermudez@hunterindustries.com>
2024-08-15 16:48:31 -0700

    Changing nozzle changes water dimensions

--- src/util/strip.ts
+++ src/util/strip.ts
@@ -17,9 +17,10 @@ import { type Products,
          type AngleDetails
          } from './huntertypes';
 import data from "./data.json";
+import { h } from 'vue';
 
 export class MPStrip extends Circle{
-  water: Rect;
+  water: MPStripwater;
   waterScale: number = 20;
   side: string = "right";
   nozzleOptions: any = [];
@@ -27,10 +28,15 @@ export class MPStrip extends Circle{
   pressure: any;
   productID: string;
   data: any;
-  nozzleMap: any =  {'left': 'Left Strip', 
+  selectedNozzle: string;
+  nozzleLookUp: any =  {'left': 'Left Strip', 
                      'right': 'Right Strip',
                      'center': 'Side Strip'
-                    }
+                    };
+  reverseNozzleLookUp: any =  {'Left Strip': 'left',
+                      'Right Strip': 'right',
+                      'Side Strip': 'center'
+                    };
   constructor(options: any){
 
     // Setting options for product
@@ -61,19 +67,20 @@ export class MPStrip extends Circle{
     this.data = data;
     this.pressure = options.pressure;
     this.productID = options.productID;
-    const id = this.productID
+    const id = this.productID;
     const productData = data[id];
     const nozzlesData = productData.nozzles;
-    const nozzle = nozzlesData[this.nozzleMap[this.side]];
-    console.log(nozzlesData, nozzle, this.pressure);
-    const pressureData = nozzle[this.pressure];
-    const height = pressureData['length'] * this.waterScale;
+    const pressures = nozzlesData[this.nozzleLookUp[this.side]];
+    const pressureData = pressures[this.roundPressure(Object.keys(pressures))];
+    const height = pressureData['height'] * this.waterScale;
     const width = pressureData['width'] * this.waterScale;
-    
-    options.width = height;
-    options.height = width;
+    this.createNozzleDictionary(productData);
+    options.width = width;
+    options.height = height;
     options.hasControls = true;
     const water = new MPStripwater(options);
+
+    this.selectedNozzle = this.reverseNozzleLookUp[this.side];
     this.water = water;
     this.waterScale = options.waterScale;
     this.side = options.side;
@@ -82,12 +89,55 @@ export class MPStrip extends Circle{
     this.canvas.add(water);
 
   }
+
+  createNozzleDictionary(data: Products){
+    for(let nozzleIdx in data.nozzles){
+      let nozzle = data.nozzles[nozzleIdx];
+      this.nozzleOptions[nozzleIdx] = {};
+      this.nozzleOptions[nozzleIdx].data = nozzle;
+      this.nozzleOptions[nozzleIdx].show = true;
+      
+    }
+  }
+
+  getSelectedNozzle(): string{
+    return this.nozzleLookUp[this.side]
+  }
+
+  setSelectedNozzle(nozzle: string): void{
+    this.selectedNozzle = this.reverseNozzleLookUp[nozzle];
+    this.side = this.reverseNozzleLookUp[nozzle];
+    this.water.setSide(this.side);
+
+    const pressures= Object.keys(this.nozzleOptions[nozzle].data);
+    const pressure = this.roundPressure(pressures);
+    const {width, height} = this.nozzleOptions[nozzle].data[pressure];
+    this.water.setWater(width, height);
+    this.water.setControls();
+  }
+
+  roundPressure(pressures: string[]): string{
+    let closestPressure = pressures[0];
+    let closestmodel = parseInt(pressures[0]);
+    let minDifference = Math.abs(parseInt(this.pressure) - closestmodel);
+    for (let i = 1; i < pressures.length; i++) {
+      const currentmodel = parseInt(pressures[i]);
+      const currentDifference = Math.abs(parseInt(this.pressure) - currentmodel);
+      if (currentDifference < minDifference) {
+        closestPressure = pressures[i];
+        minDifference = currentDifference;
+      }
+    }
+    return closestPressure;
+  }
 }
 
 export class MPStripwater extends Rect{
   product: MPStrip; 
   pressure: string;
   side: string;
+  canvas: Canvas;
+  waterScale: number = 20;
 
   constructor(options: any){
     options.fill = 'rgba(0, 0, 255, .2)';
@@ -95,21 +145,65 @@ export class MPStripwater extends Rect{
     this.product = options.product;
     this.pressure = '';
     this.side = options.side;
-    console.log(this.side)
-    this.controls.tl = new Control({
-      x: -0.5,
-      y: -0.5,
-      offsetY: 0,
-      offsetX: 0,
-      actionName: 'scaleRotate',
-      cursorStyle: 'pointer',
-      withConnection: true,
-      actionHandler(eventData, transform, x, y) {
-        controlsUtils.rotationWithSnapping(eventData, transform, x, y);
-        controlsUtils.scalingEqually(eventData, transform, x, y);
-        return true;
-      }
+    this.canvas = options.canvas;
+    this.setControls();
+    this.product.on({
+      'moving': (e) => {this.set({left: this.product.left, top: this.product.top});   
+                        this.setCoords();},
+      "mousedblclick": (e) => {console.log(this.product)},
     });
+  }
+
+  setControls(){
+    if(this.side === 'right'){
+      this.controls.tl = new Control({
+        x: -0.5,
+        y: -0.5,
+        offsetY: 0,
+        offsetX: 0,
+        actionName: 'scaleRotate',
+        cursorStyle: 'pointer',
+        withConnection: true,
+        actionHandler(eventData, transform, x, y) {
+          controlsUtils.rotationWithSnapping(eventData, transform, x, y);
+          controlsUtils.scalingEqually(eventData, transform, x, y);
+          return true;
+        }
+      });
+    }
+    else if(this.side === 'left'){
+      this.controls.tr = new Control({
+        x: 0.5,
+        y: -0.5,
+        offsetY: 0,
+        offsetX: 0,
+        actionName: 'scaleRotate',
+        cursorStyle: 'pointer',
+        withConnection: true,
+        actionHandler(eventData, transform, x, y) {
+          controlsUtils.rotationWithSnapping(eventData, transform, x, y);
+          controlsUtils.scalingEqually(eventData, transform, x, y);
+          return true;
+        }
+      });
+    }
+    else{
+      console.log("center")
+      this.controls.mt = new Control({
+        x: 0,
+        y: -0.5,
+        offsetY: 0,
+        offsetX: 0,
+        actionName: 'scaleRotate',
+        cursorStyle: 'pointer',
+        withConnection: true,
+        actionHandler(eventData, transform, x, y) {
+          controlsUtils.rotationWithSnapping(eventData, transform, x, y);
+          // controlsUtils.scalingEqually(eventData, transform, x, y);
+          return true;
+        }
+      });
+    }
 
     this.setControlsVisibility({
       tr: this.side === 'left',
@@ -120,15 +214,25 @@ export class MPStripwater extends Rect{
       mb: false,
       ml: false,
       mr: false,
-      mtr: false, // Hide the default rotation control
+      mtr: false,
     });
+    this.set({originX: this.side});
+    this.setCoords();
+    this.canvas.renderAll();
+  }
 
-    this.product.on({
-      'moving': (e) => {this.set({left: this.product.left, top: this.product.top});   
-                        this.setCoords();},
-      "mousedblclick": (e) => {console.log(this.product)},
-      // 'selected': (e) => {this.showControls(true)},
-      // 'deselected': (e) => {this.showControls(false)},
+  setSide(side: string): void{
+    this.side = side;
+  }
+
+  setWater(width: number, height: number): void{
+    this.set({
+      scaleX: 1,
+      scaleY: 1,
+      width: width*this.waterScale,
+      height: height*this.waterScale
     });
+    this.setCoords();
+    this.canvas.renderAll();
   }
 }
